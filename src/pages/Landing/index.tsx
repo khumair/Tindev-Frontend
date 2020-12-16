2eaecc07d5c510a5ad402564a14f74a57149615b
Tsaikkari <trospe@gmx.de>
2020-12-17 00:00:06 +0100

    Adding login and register routes

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import NavBar from '../../components/NavBar'
 import LandingTop from '../../components/LandingTop';
 import LandingBottom from '../../components/LandingBottom';
 
 const Landing = () => (
 	<>
+		<NavBar />
 		<LandingTop />
 		<LandingBottom />
 	</>
