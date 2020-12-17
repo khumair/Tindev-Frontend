26b26c3842ffeb555f82e2efb9e8ed22ed4cd9e7
Gabriel <olaniyi_g@hotmail.com>
2020-12-17 12:22:11 +0100

    still on editProfile

--- src/components/CustomSvgIcon/index.tsx
+++ src/components/CustomSvgIcon/index.tsx
@@ -3,7 +3,7 @@ type svg = {
 };
 
 const CustomSvgIcon = ({ img }: svg) => (
-	<img src={img} alt='bottom-right-svg' className='svg-icon hide-sm' />
+	<img src={img} alt='bottom-right-svg' className='svg-icon' />
 );
 
 export default CustomSvgIcon;
