2c3f13dbb089250f14bb10897fe2f546aac54969
Tsaikkari <trospe@gmx.de>
2020-12-21 11:12:06 +0100

    Removing email verify

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,7 +4,6 @@ import Landing from './pages/Landing';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import EditProfile from './pages/EditProfile';
-import EmailVerification from './pages/EmailVerification';
 import PWReset from './pages/PWReset';
 
 const Routes = () => (
@@ -12,7 +11,6 @@ const Routes = () => (
 		<Route exact path='/' component={Landing} />
 		<Route path='/login' component={Login} />
 		<Route path='/register' component={Register} />
-		<Route path='/verify' component={EmailVerification} />
 		<Route path='/edit-jobseeker-profile' component={EditProfile} />
 		<Route path='/reset-password' component={PWReset} />
 	</Switch>
