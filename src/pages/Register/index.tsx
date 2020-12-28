adc69bba89bbba787ec5a9216e167db4314d3fe6
Tsaikkari <kirsi.trospe@gmail.com>
2020-12-28 19:47:05 +0100

    Controlled form

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -10,33 +10,24 @@ import CustomButton from '../../components/CustomButton'
 import Loader from '../../components/Loader'
 import Message from '../../components/Message'
 import { registerEmployerRequest } from '../../redux/actions'
-import { AppState } from '../../types'
+import { AppState } from '../../redux/types'
+import FormContainer from '../../components/FormContainer'
 
 const Register = () => {
-  const [validated, setValidated] = useState(false)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
   const [message, setMessage] = useState('')
 
-  // const employerRegister = useSelector((state: AppState) => state.employerRegister)
-  // const { loading, error, credentials } = employerRegister
+  const employer = useSelector((state: AppState) => state.employer)
+  const { loading, error, credentials } = employer
 
   const dispatch = useDispatch()
 
   const handleSubmit = (event: React.FormEvent) => {
     console.log('clicked')
-    // if (form.checkValidity() === false) {
     event.preventDefault()
-    //   event.stopPropagation()
-    // }
 
-    // setValidated(true)
-
-    const credentials = {
-      email: email,
-      password: password,
-    }
     console.log('credentials', credentials)
     if (password !== confirmPassword) {
       setMessage('Passwords do not match')
@@ -47,72 +38,62 @@ const Register = () => {
 
   return (
     <>
-      <HalfCircle inputText="Welcome" />
-      <h3 className="text-center my-5">Sign up</h3>
-      {message && <Message variant="danger">{message}</Message>}
-      {/* {error && <Message variant='danger'>{error}</Message>}
-			{loading && <Loader />} */}
-      <Form
-        noValidate
-        validated={validated}
-        onSubmit={handleSubmit}
-        className="container"
-      >
-        <Form.Row>
-          <Form.Group as={Col} sm="8" controlId="validationCustomEmail">
-            <Form.Control
-              required
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-            />
-            <Form.Control.Feedback type="invalid">
-              eMail is required
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Form.Row>
+      <FormContainer>
+        <HalfCircle inputText="Welcome" />
+        <h3 className="text-center my-5">Sign up</h3>
+        {message && <Message variant="danger">{message}</Message>}
+        {error && <Message variant="danger">{error}</Message>}
+        {loading && <Loader />}
+        <Form onSubmit={handleSubmit} className="container">
+          <Form.Row>
+            <Form.Group as={Col} controlId="validationCustomEmail">
+              <Form.Control
+                required
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+              <Form.Control.Feedback type="invalid">
+                eMail is required
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
 
-        <Form.Row>
-          <Form.Group as={Col} sm="8" controlId="validationCustomPassword">
-            <Form.Control
-              required
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-            />
-            <Form.Control.Feedback type="invalid">
-              Password is required
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} controlId="validationCustomPassword">
+              <Form.Control
+                required
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+              <Form.Control.Feedback type="invalid">
+                Password is required
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
 
-        <Form.Row>
-          <Form.Group
-            as={Col}
-            sm="8"
-            controlId="validationCustomConfirmPassword"
-          >
-            <Form.Control
-              required
-              type="password"
-              placeholder="Confirm password"
-              value={confirmPassword}
-              onChange={e => setConfirmPassword(e.target.value)}
-            />
-            <Form.Control.Feedback type="invalid">
-              Passwords does not match!
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} controlId="validationCustomConfirmPassword">
+              <Form.Control
+                required
+                type="password"
+                placeholder="Confirm password"
+                value={confirmPassword}
+                onChange={e => setConfirmPassword(e.target.value)}
+              />
+            </Form.Group>
+          </Form.Row>
 
-        <CustomButton text="Submit" className="my-3" />
+          <CustomButton text="Submit" className="my-3" />
 
-        <p>
-          Already a member? <Link to="/login">Sign In</Link>
-        </p>
-      </Form>
+          <p>
+            Already a member? <Link to="/login">Sign In</Link>
+          </p>
+        </Form>
+      </FormContainer>
       <CustomSvgIcon img={starsLady} />
     </>
   )
--- src/redux/saga/employer.ts
+++ src/redux/saga/employer.ts
@@ -1,19 +1,22 @@
-import { put, all, takeLatest, select } from 'redux-saga/effects';
-import axios from 'axios';
+import { put, takeLatest, select } from 'redux-saga/effects'
+import axios from 'axios'
+import { AppState } from '../types'
 
-import { registerEmployerSuccess } from '../../redux/actions/employer';
+import { registerEmployerSuccess } from '../../redux/actions/employer'
 
-function* registerEmployerSaga(credentials: Credential) {
+const credentials = (state: AppState) => state.employer.credentials
+function* registerEmployerSaga() {
+  const credentialData = yield select(credentials)
   try {
-    const req = yield axios.post('/employer', { credentials });
-    yield put(registerEmployerSuccess(req.data));
+    const req = yield axios.post('/employer', { credentialData })
+    yield put(registerEmployerSuccess(req.data))
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
 const sagaWatcher = [
   takeLatest('REGISTER_EMPLOYER_REQUEST', registerEmployerSaga),
-];
+]
 
-export default sagaWatcher;
+export default sagaWatcher
