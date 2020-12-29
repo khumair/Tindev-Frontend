588e3b72835cb8be1514f6f29ab1287e9d65708d
Tsaikkari <kirsi.trospe@gmail.com>
2020-12-29 14:04:59 +0100

    Adding role buttons

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { Form, Col, Row } from 'react-bootstrap'
+import { Form, Col, Button } from 'react-bootstrap'
 
 import HalfCircle from '../../components/HalfCircle'
 import CustomSvgIcon from '../../components/CustomSvgIcon'
@@ -18,24 +18,24 @@ import FormContainer from '../../components/FormContainer'
 import './Register.scss'
 
 const Register = () => {
-  const [isJobSeeker, setIsJobSeeker] = useState({})
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
   const [message, setMessage] = useState('')
+  const [role, setRole] = useState({})
 
   const employer = useSelector((state: AppState) => state.employer)
   const jobSeeker = useSelector((state: AppState) => state.jobSeeker)
-  const { loading, error } = employer || jobSeeker
+  const { loading, error } = jobSeeker || employer
 
   const dispatch = useDispatch()
 
-  const handleChange = () => {
-    if (!isJobSeeker) {
-      setIsJobSeeker(true)
-    } else {
-      setIsJobSeeker(false)
-    }
+  const handleCompany = () => {
+    setRole(employer)
+  }
+
+  const handleSeeker = () => {
+    setRole(jobSeeker)
   }
 
   const handleSubmit = (event: React.FormEvent) => {
@@ -43,9 +43,9 @@ const Register = () => {
 
     if (password !== confirmPassword) {
       setMessage('Passwords do not match')
-    } else if (isJobSeeker) {
+    } else if (role === employer) {
       dispatch(registerEmployerRequest(email, password))
-    } else {
+    } else if (role === jobSeeker) {
       dispatch(registerJobSeekerRequest(email, password))
     }
   }
@@ -59,27 +59,10 @@ const Register = () => {
         {error && <Message variant="danger">{error}</Message>}
         {loading && <Loader />}
         <Form onSubmit={handleSubmit} className="container">
-          <Form.Group as={Row}>
-            <Form.Label as="legend" column sm={12}>
-              Are you a jobseeker?
-            </Form.Label>
-            <Col as={Row} sm={10} className="mt-2">
-              <Form.Check
-                type="radio"
-                label="Yes"
-                name="formVerticalRadios"
-                defaultChecked={!isJobSeeker}
-                className="pr-4"
-                onChange={handleChange}
-              />
-              <Form.Check
-                type="radio"
-                label="No"
-                name="formVerticalRadios"
-                onChange={handleChange}
-              />
-            </Col>
-          </Form.Group>
+          <Button className="employer-role" onClick={handleCompany}>
+            I am an employer
+          </Button>
+          <Button onClick={handleSeeker}>I am a jobseeker</Button>
           <Form.Row>
             <Form.Group as={Col} controlId="validationCustomEmail">
               <Form.Control
@@ -90,7 +73,7 @@ const Register = () => {
                 onChange={e => setEmail(e.target.value)}
               />
               <Form.Control.Feedback type="invalid">
-                eMail is required
+                Email is required
               </Form.Control.Feedback>
             </Form.Group>
           </Form.Row>
