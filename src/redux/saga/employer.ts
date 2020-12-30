37d0f5651f3ffcd57a98d3da566d96467eae7f7f
Dilip Poudel <dilippoudel578@gmail.com>
2020-12-31 00:11:30 +0200

    saga function written

--- src/redux/saga/employer.ts
+++ src/redux/saga/employer.ts
@@ -1,19 +1,36 @@
-import { put, all, takeLatest, select } from 'redux-saga/effects';
-import axios from 'axios';
+import { REGISTER_EMPLOYER_REQUEST, JOB_POST_REQUEST } from '../types'
+import { registerJobPostSuccess } from './../actions/jobpost'
+import { AppState } from './../types'
+import { put, takeLatest, select } from 'redux-saga/effects'
+import axios from 'axios'
 
-import { registerEmployerSuccess } from '../../redux/actions/employer';
+import { registerEmployerSuccess } from '../../redux/actions/employer'
+
+const jobPostFormData = (state: AppState) => state.employer.jobPost
 
 function* registerEmployerSaga(credentials: Credential) {
   try {
-    const req = yield axios.post('/employer', { credentials });
-    yield put(registerEmployerSuccess(req.data));
+    const req = yield axios.post('/employer', { credentials })
+    yield put(registerEmployerSuccess(req.data))
   } catch (error) {
-    console.log(error);
+    console.log(error)
+  }
+}
+
+function* creatingJobPostSaga() {
+  try {
+    const job = yield select(jobPostFormData)
+    const res = yield axios.post('/jobpost', job)
+    console.log(res)
+    yield put(registerJobPostSuccess())
+  } catch (e) {
+    console.log(e)
   }
 }
 
 const sagaWatcher = [
-  takeLatest('REGISTER_EMPLOYER_REQUEST', registerEmployerSaga),
-];
+  takeLatest(REGISTER_EMPLOYER_REQUEST, registerEmployerSaga),
+  takeLatest(JOB_POST_REQUEST, creatingJobPostSaga),
+]
 
-export default sagaWatcher;
+export default sagaWatcher
