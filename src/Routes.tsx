b55b484183e61592be483e6ed7056807e305c365
Tsaikkari <kirsi.trospe@gmail.com>
2021-01-06 18:31:30 +0100

    Remove useState

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,7 +3,6 @@ import { Switch, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import JobSeeker from './components/MatchCardJobseeker'
 import PWReset from './pages/PWReset'
 import ViewJobseeker from './pages/ViewJobseeker'
 import CompanyProfile from './pages/CompanyProfile'
@@ -14,7 +13,6 @@ const Routes = () => (
     <Route exact path="/" component={Landing} />
     <Route exact path="/login" component={Login} />
     <Route exact path="/register" component={Register} />
-    <Route exact path="/jobseeker/match" component={JobSeeker} />
     <Route exact path="/reset-password" component={PWReset} />
     <Route exact path="/jobseeker/view" component={ViewJobseeker} />
     <Route exact path="/company/profile" component={CompanyProfile} />
