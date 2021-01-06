ed3866ad6c901d6c6336de10804ae32c17d29f96
Tsaikkari <kirsi.trospe@gmail.com>
2021-01-06 14:30:17 +0100

    Removing extra code

--- src/pages/CompanyProfile/index.tsx
+++ src/pages/CompanyProfile/index.tsx
@@ -18,45 +18,6 @@ const CompanyProfile = () => {
         formComponent={<CompanyProfileForm />}
         matchComponent={<MatchCardCompany match={match} />}
       />
-      {/* <div className="py-3">
-        <Tab.Container defaultActiveKey="first">
-          <Row>
-            <Col sm={3}>
-              <Nav className="flex-column nav-bar">
-                <Nav.Item>
-                  <Nav.Link eventKey="first">Post</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="second">Match</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="third">Chat</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Col>
-            <Col sm={9} className="tab-content">
-              <Tab.Content>
-                <Tab.Pane eventKey="first">
-                  <CompanyProfileForm />
-                </Tab.Pane>
-                <Tab.Pane eventKey="second">
-                  {jobseekerMatches.map(jobseeker => {
-                    return (
-                      <MatchCardCompany
-                        key={jobseeker.lastName}
-                        firstName={jobseeker.firstName}
-                        lastName={jobseeker.lastName}
-                        skills={jobseeker.skills}
-                      />
-                    )
-                  })}
-                </Tab.Pane>
-                <Tab.Pane eventKey="third"></Tab.Pane>
-              </Tab.Content>
-            </Col>
-          </Row>
-        </Tab.Container>
-      </div> */}
     </>
   )
 }
