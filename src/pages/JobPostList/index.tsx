4de4049408fe86695fdef8a14e439a1a79bbe682
Tsaikkari <kirsi.trospe@gmail.com>
2021-01-16 17:48:30 +0100

    Testing

--- src/pages/JobPostList/index.tsx
+++ src/pages/JobPostList/index.tsx
@@ -6,11 +6,30 @@ import './JobPostList.scss'
 import { AppState } from '../../redux/types'
 
 const JobPostList = () => {
+  // testing
+  //   const jobPosts = [
+  //     {
+  //        id: 1,
+  //        title: "fullstack developer",
+  //        jobDescription: "full stack development",
+  //        seniority: "Junior",
+  //        startingDate: "{\"day\":31,\"month\":1,\"year\":2021}",
+  //        skills: ["javascript", "reactjs", "nodejs"]
+  //     },
+  //     {
+  //        id: 2,
+  //        title: "fullstack developer",
+  //        jobDescription: "job",
+  //        seniority: "Junior",
+  //        startingDate: "{\"day\":29,\"month\":1,\"year\":2021}",
+  //        skills: ["javascript", "reactjs", "nodejs"]
+  //     },
+  //   ]
+
   const jobPosts = useSelector((state: AppState) => state.employer.jobPosts)
 
   return (
     <>
-      {/* <h2 className="job-posts">Job Posts</h2> */}
       <div className="job-post-card">
         {jobPosts.map(jp => {
           return (
