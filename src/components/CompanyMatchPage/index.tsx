e9557c63033b35b3dd00e21bbf9d103213604f8b
khumair <khawja.umair@gmail.com>
2021-01-19 00:10:29 +0200

    bug fix

--- src/components/CompanyMatchPage/index.tsx
+++ src/components/CompanyMatchPage/index.tsx
@@ -1,45 +1,42 @@
-import * as React from "react";
-import { Tab, Row, Col, Nav,Form, InputGroup, FormControl, Button} from "react-bootstrap";
+import * as React from 'react'
+import { Tab, Row, Col, Nav } from 'react-bootstrap'
 import './style.scss'
 import MatchCard from './MatchCard'
-import SearchBarCont from './SearchBarCont'
 import JobPost from '../JobPost'
 const CompanyMatchPage = () => {
   return (
-  <>
-
-  <div>
-  <Tab.Container  defaultActiveKey="first">
-  <Row>
-    <Col sm={3}>
-      <Nav className="flex-column nav-bar">
-        <Nav.Item>
-          <Nav.Link eventKey="first">Post</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="second">Match</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="third">Chat</Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </Col>
-    <Col sm={9} className="tab-content">
-      <Tab.Content>
-        <Tab.Pane eventKey="first">
-            <JobPost />
-        </Tab.Pane>
-        <Tab.Pane eventKey="second">
-        <MatchCard />
-        </Tab.Pane>
-        <Tab.Pane eventKey="third">
-        </Tab.Pane>
-      </Tab.Content>
-    </Col>
-  </Row>
-</Tab.Container>
-</div>
-  </>
-  );
-};
-export default CompanyMatchPage;
+    <>
+      <div>
+        <Tab.Container defaultActiveKey="first">
+          <Row>
+            <Col sm={3}>
+              <Nav className="flex-column nav-bar">
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Post</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Match</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Chat</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Col>
+            <Col sm={9} className="tab-content">
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  <JobPost />
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  <MatchCard />
+                </Tab.Pane>
+                <Tab.Pane eventKey="third"></Tab.Pane>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
+      </div>
+    </>
+  )
+}
+export default CompanyMatchPage
